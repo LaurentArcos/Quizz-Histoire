4b2156424e9ebe28550148396cec0585c5aca1cc
LaurentArcos <laurent.arcos@gmail.com>
2023-02-16 16:58:28 +0100

    TextFieldComp component created

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,13 +1,20 @@
 import { Button } from "@mui/material";
 import { Box } from "@mui/system";
 import SelectField from "../components/SelectField";
+import TextFieldComp from "../components/TextFieldComp";
 
 const Settings = () => {
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+  
   return (
-<form onSubmit=h>
+<form onSubmit={handleSubmit}>
   <SelectField label='Category' />
   <SelectField label='Difficulty' />
   <SelectField label='Type' />
+  <TextFieldComp/>
   <Box mt={3} width="100%" >
     <Button fullWidth variant="contained" type="submit">
       Get Started
