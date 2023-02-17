60d183839bc8f5480ee8fc82d4a5506c4396be47
LaurentArcos <laurent.arcos@gmail.com>
2023-02-17 15:43:21 +0100

    useAxios added to Settings

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
-axios.defaults.baseURL = "https://opentdb.com/"
+axios.defaults.baseURL = "https://opentdb.com"
 
 const useAxios = () => {
   
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,9 +1,28 @@
-import { Button } from "@mui/material";
+import { Button, CircularProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import SelectField from "../components/SelectField";
 import TextFieldComp from "../components/TextFieldComp";
+import useAxios from "../hooks/useAxios";
 
 const Settings = () => {
+
+  const { response, error, loading } = useAxios({url:"/api_category.php"})
+
+  if (loading) {
+    return (
+      <Box mt={20}>
+        <CircularProgress />
+      </Box>
+    )
+  }
+
+  if (error) {
+    return (
+      <Typography variant="6" mt={20} color="red">
+        Something went wrong
+      </Typography>
+    )
+  }
 
   const handleSubmit = e => {
     e.preventDefault();
