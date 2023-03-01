3726607353742559ba9625cc92eea00dde368421
LaurentArcos <laurent.arcos@gmail.com>
2023-03-01 16:55:34 +0100

    page des questions créée

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,8 +1,20 @@
+import { Button, Typography } from '@mui/material'
+import { Box } from '@mui/system'
 import React from 'react'
 
 const Questions = () => {
   return (
-    <div>Questions</div>
+    <Box>
+      <Typography>Question 1</Typography>
+      <Typography mt={5}>This is the question</Typography>
+      <Box mt={2}>
+        <Button variant="contained">Answer 1</Button>
+      </Box>
+      <Box mt={2}>
+        <Button variant="contained">Answer 2</Button>
+      </Box>
+      <Box mt={5}>Score 2 / 5</Box>
+    </Box>
   )
 }
 
