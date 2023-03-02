b724a4e49bfb9b0968f88074992c5cb508577c3e
LaurentArcos <laurent.arcos@gmail.com>
2023-03-02 17:45:46 +0100

    dynamisation et mise en style de la page finalscreen

--- src/pages/FinalScreen.js
+++ src/pages/FinalScreen.js
@@ -1,8 +1,27 @@
+import { Button, Typography } from '@mui/material'
+import { Box } from '@mui/system'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+import { handleAmountChange, handleScoreChange } from '../redux/actions'
 
 const FinalScreen = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {score} = useSelector(state => state);
+
+  const handleBackToSettings = () => {
+    dispatch(handleScoreChange(0));
+    dispatch(handleAmountChange(50));
+    navigate('/');
+  }
+  
   return (
-    <div>FinalScreen</div>
+    <Box mt={3}>
+      <Typography variant="h3" fontWeight="bold" mb={3}>Final Score {score}</Typography>
+      <Button onClick={handleBackToSettings} variant="outlined">back to settings !</Button>
+    </Box>
   )
 }
 
