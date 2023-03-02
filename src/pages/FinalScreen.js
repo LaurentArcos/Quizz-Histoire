a951792b7341571310ac2c2607e4c4b766d05b40
LaurentArcos <laurent.arcos@gmail.com>
2023-03-02 18:50:14 +0100

    changement import use navigate

--- src/pages/FinalScreen.js
+++ src/pages/FinalScreen.js
@@ -2,7 +2,7 @@ import { Button, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router'
+import { useNavigate } from 'react-router-dom'
 import { handleAmountChange, handleScoreChange } from '../redux/actions'
 
 const FinalScreen = () => {
@@ -13,12 +13,13 @@ const FinalScreen = () => {
 
   const handleBackToSettings = () => {
     dispatch(handleScoreChange(0));
+    dispatch(handleAmountChange(10));
     navigate('/');
   }
   
   return (
     <Box mt={3}>
-      <Typography variant="h3" fontWeight="bold" mb={3}>Final Score : {score}</Typography>
+      <Typography variant="h3" fontWeight="bold" mb={3}>Final Score {score}</Typography>
       <Button onClick={handleBackToSettings} variant="outlined">back to settings !</Button>
     </Box>
   )
